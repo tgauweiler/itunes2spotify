0205526c8a13e181653a13b145f29fce1de5adb2
William Reardon <wdr1@pobox.com>
2018-06-25 21:37:03 -0700

    Cruft removal

--- itunes2spotify.py
+++ itunes2spotify.py
@@ -10,21 +10,11 @@ iTunes2spotifyMapping = {"Name": "track", "Artist": "artist", "Album": "album"}
 spotifyFunctions.iTunes2spotifyMapping = iTunes2spotifyMapping
 itunesFunctions.iTunes2spotifyMapping = iTunes2spotifyMapping
 
-
 def main():
     playlistName = itunesFunctions.iTunesXML2PlaylistName("liveinlondon.xml")
     tracks = itunesFunctions.itunesXML2PythonDict("liveinlondon.xml")
 
     spotifyFunctions.addTracksToPlaylist(playlistName, tracks)
-    # playlistId = spotifyFunctions.createPlaylist(playlistName)
-    # songID = spotifyFunctions.findSpotifyURI(testDict)
-    # spotifyFunctions.addSpotifyURIstoPlaylist(playlistId, [ songID ])
-
-
-def NOTmain():
-    playlistName = "Test " + str(int(time.time()))
-    print playlistName
-    testDict = {'album': 'Live In London', 'track': 'Dance Me To The End Of Love', 'artist': 'Leonard Cohen'}
 
 if __name__ == "__main__":
     main()
